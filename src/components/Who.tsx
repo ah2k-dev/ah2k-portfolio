bb70220d03b8b32370da0ce388bb58df3f31d4cd
ahmedhassan2k <ah2k.dev@gmail.com>
2023-02-19 13:43:02 +0500

    Added download resume button and minor changes

--- src/components/Who.tsx
+++ src/components/Who.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 // import Canvas from "./Canvas";
-
+import { BsDownload } from "react-icons/bs";
 const Who: FC = () => {
   return (
     <div
@@ -35,8 +35,41 @@ const Who: FC = () => {
         </h1>
 
         <p data-scroll className="text-lg text-gray-200" id="story">
-        I am Ahmed Hassan, a software engineer, MERN stack developer with a BS degree, and I have a passion for creating immersive digital experiences. Over the past two years, I have gained extensive industry experience and have become proficient in a wide range of technologies such as React, Node, MongoDB, SQL, PostgreSQL, ExpressJS, AntD, MUI, Bootstrap, and AWS. As an expert in my field, I am dedicated to building innovative solutions that are not limited to the ordinary, using my skills to develop impactful software. From web applications to full-stack development, I apply my creativity and attention to detail to every project, ensuring that the end product is a work of art that exceeds all expectations.
+          I am Ahmed Hassan, a software engineer, MERN stack developer with a BS
+          degree, and I have a passion for creating immersive digital
+          experiences. Over the past two years, I have gained extensive industry
+          experience and have become proficient in a wide range of technologies
+          such as React, Node, MongoDB, SQL, PostgreSQL, ExpressJS, AntD, MUI,
+          Bootstrap, and AWS. As an expert in my field, I am dedicated to
+          building innovative solutions that are not limited to the ordinary,
+          using my skills to develop impactful software. From web applications
+          to full-stack development, I apply my creativity and attention to
+          detail to every project, ensuring that the end product is a work of
+          art that exceeds all expectations.
         </p>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            marginTop: "20px",
+          }}
+        >
+          <a
+            // href={project.demo}
+            download="resume.pdf"
+            href="/resume.pdf"
+            style={{
+              width: "fit-content",
+              justifySelf: "flex-end",
+            }}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="h-[45px] px-[15px] text-white rounded transition duration-300 flex items-center gap-[10px] bg-[#1876d2] hover:bg-[#2884e0]"
+          >
+            <BsDownload size={25} />
+            <span> Resume </span>
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,6 @@ import { LazyMotion, domAnimation } from "framer-motion";
 
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -13,47 +12,23 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <link
           rel="shortcut icon"
-          href="/rounded-avatar.png"
+          href="/dp.jpg"
           type="image/x-icon"
         />
 
-        <title>{`AH2K-DEV Portfolio`}</title>
+        <title>{`Ahmed's Portfolio`}</title>
 
-        <meta name="title" content="NAPTheDev's Portfolio" />
-        <meta name="description" content="Nguyen Anh Phong's Portfolio" />
+        <meta name="title" content="Ahmed's Portfolio" />
+        <meta name="description" content="Ahmed Hassan's Portfolio" />
 
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="NAPTheDev's Portfolio" />
-        <meta
-          property="og:description"
-          content="Nguyen Anh Phong's Portfolio"
-        />
+        <meta property="og:title" content="Ahmed's Portfolio" />
+        <meta property="og:description" content="Ahmed Hassan's Portfolio" />
         <meta property="og:image" content="/avatar.jpg" />
-
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:title" content="NAPTheDev's Portfolio" />
-        <meta
-          property="twitter:description"
-          content="Nguyen Anh Phong's Portfolio"
-        />
-        <meta property="twitter:image" content="/avatar.jpg" />
       </Head>
       <LazyMotion features={domAnimation}>
         <Component {...pageProps} />
       </LazyMotion>
-      <Script
-        strategy="lazyOnload"
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-      ></Script>
-      <Script id="google-analytics" strategy="lazyOnload">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-        `}
-      </Script>
     </>
   );
 }
